7e5fd07abb927d8404b0b07065724f5416d6002f
David Rjoian <95087967+David-Rjoian@users.noreply.github.com>
2023-01-30 00:41:14 +0400

    29 Jan JS

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-window.onload = function () {
-    let button = document.getElementById("button");
-    let menu = document.getElementById("menu");
-}
-
 let counter = 0;
 
 button.onclick = function () {
@@ -49,20 +44,115 @@ prog.addEventListener("click", function () {
     progTarget.scrollIntoView({ behavior: "smooth", block: "start" });
 });
 
+let soft = document.getElementById("linkSoft");
+let softTarget = document.getElementById("softTarget");
+
+soft.addEventListener("click", function () {
+    softTarget.scrollIntoView({ behavior: "smooth", block: "start" });
+});
+
+
+
 let themeButton = document.getElementById("themeButton");
 let themeNow = 0;
 
 themeButton.onclick = function () {
     if (themeNow == 0) {
         document.getElementById("theme").href = "dark.css";
+        document.getElementById("L").style.display = "none";
+        document.getElementById("D").style.display = "block";
         themeNow += 1;
     } else {
         document.getElementById("theme").href = "light.css";
+        document.getElementById("L").style.display = "block";
+        document.getElementById("D").style.display = "none";
         themeNow -= 1;
     };
 };
 
-if (window.innerWidth < 1300) {
-    menu.style.display = "none";
-    themeButton.style.display = "none";
+let descOne = document.getElementById("descOne");
+let blockOne = document.getElementById("blockOne");
+blockOne.style.display = "none";
+
+descOne.onclick = function () {
+    if (blockOne.style.display == "none") {
+        blockOne.style.display = "block";
+    } else {
+        blockOne.style.display = "none";
+    };
+};
+
+let descTwo = document.getElementById("descTwo");
+let blockTwo = document.getElementById("blockTwo");
+blockTwo.style.display = "none";
+
+descTwo.onclick = function () {
+    if (blockTwo.style.display == "none") {
+        blockTwo.style.display = "block";
+    } else {
+        blockTwo.style.display = "none";
+    };
+};
+
+let descThree = document.getElementById("descThree");
+let blockThree = document.getElementById("blockThree");
+blockThree.style.display = "none";
+
+descThree.onclick = function () {
+    if (blockThree.style.display == "none") {
+        blockThree.style.display = "block";
+    } else {
+        blockThree.style.display = "none";
+    };
+};
+
+let descFour = document.getElementById("descFour");
+let blockFour = document.getElementById("blockFour");
+blockFour.style.display = "none";
+
+descFour.onclick = function () {
+    if (blockFour.style.display == "none") {
+        blockFour.style.display = "block";
+    } else {
+        blockFour.style.display = "none";
+    };
+};
+
+let descFive = document.getElementById("descFive");
+let blockFive = document.getElementById("blockFive");
+blockFive.style.display = "none";
+
+descFive.onclick = function () {
+    if (blockFive.style.display == "none") {
+        blockFive.style.display = "block";
+    } else {
+        blockFive.style.display = "none";
+    };
+};
+
+let descSix = document.getElementById("descSix");
+let blockSix = document.getElementById("blockSix");
+blockSix.style.display = "none";
+
+descSix.onclick = function () {
+    if (blockSix.style.display == "none") {
+        blockSix.style.display = "block";
+    } else {
+        blockSix.style.display = "none";
+    };
+};
+
+window.onload = function () {
+    let button = document.getElementById("button");
+    let menu = document.getElementById("menu");
+};
+
+window.onload = function () {
+    if (window.innerWidth < 1920) {
+        menu.style.display = "none";
+        themeButton.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+        themeButton.style.display = "flex";
+    }
 };
